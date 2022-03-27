f2ff9c59e48ef95f3e1bc4836b5c058ce3dba961
ALE OLIVARES <ale_ema94@hotmail.com>
2022-03-26 21:02:22 -0300

    clase 04

--- React JS - Coder House/ecommerce-react/src/App.js
+++ React JS - Coder House/ecommerce-react/src/App.js
@@ -1,13 +1,19 @@
 
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar";
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
+      <Router>
+        <NavBar/>
+      </Router>
+
+      
+     <ItemListContainer greeting = {'Ecommerce-React'} /> 
     </div>
   );
 }
--- React JS - Coder House/ecommerce-react/src/components/NavBar.js
+++ React JS - Coder House/ecommerce-react/src/components/NavBar.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react';
-import{Container,Navbar,Nav,NavDropdown} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import CartWidget from './CartWidget/CartWidget';
 
-export default class NavBar extends Component {
-  render() {
+const navbar = () => {
+  
     return (
-      <div> 
-          <Navbar bg="dark" variant ={"dark"} expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Ecommerce-React</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#inicio">Inicio</Nav.Link>
-        <Nav.Link href="#carrito">Carrito</Nav.Link>
-        <Nav.Link href="#contacto">Contacto</Nav.Link>
-        <NavDropdown title="Marcas" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-     </div>
+      
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-info">
+        <div className="container-fluid">
+       <Link to='/'>
+         <img src='./logoecommerce.svg' width= '90'/>
+       </Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav m-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to= '/' >Inicio</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link" to= '/' >Marcas</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link" to= '/' >Carrito</Link>
+        </li>
+        <li className="nav-item">
+        <Link className="nav-link" to= '/' >Quienes Somos?</Link>
+        </li>
+      </ul>
+      <CartWidget/>
+    </div>
+  </div>
+</nav>
+
+
+      </div>
+
+      
+
+
+
     );
   }
-}
+
+export default navbar
